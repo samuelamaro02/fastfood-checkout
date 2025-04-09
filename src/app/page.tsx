58006eca7406ce8bcd357da4ef03a8d8a2b4df2e
samuelamaro02 <samuelamaro261@gmail.com>
2025-04-09 14:36:58 -0300

    chore: add eslint sort plugin add prettier taiwlind plugin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "../components/ui/button";
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline bg-red-500 text-white p-4">
+      <h1 className="bg-red-500 p-4 text-3xl font-bold text-white underline">
         Hello world!
       </h1>
       <Button>Click me</Button>
