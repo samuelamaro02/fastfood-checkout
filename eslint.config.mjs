c4f0986328d3aeafcdcfffa3d351ad215a1ec0b5
Samuel Amaro <134892999+samuelamaro02@users.noreply.github.com>
2025-04-10 20:22:41 +0000

    chore: att package.json and eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,9 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
+
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
